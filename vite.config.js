1906fe75e77cc0962c0758a68bd3ea7a69f6c6c7
Guransh Dhaliwal <guranshcodes@outlook.com>
2026-06-02 19:51:41 -0400

    fix vite base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,14 @@
-
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
-
 export default defineConfig({
-  base: './',
-  logLevel: 'error', 
+  base: '/Finanical-Literacy/',
+  logLevel: 'error',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
   },
-  plugins: [
-    react(),
-  ]
-});
+  plugins: [react()],
+})
